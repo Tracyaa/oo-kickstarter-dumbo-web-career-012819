0e27d434da67df85b258e69c20c141580271e0b9
Tracy Ho <tracyho0867@gmail.com>
2019-02-01 17:09:09 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,15 +1,15 @@
 def class Project
-  # attr_accessor :backers
+  attr_accessor :backers
   attr_reader :title
 
 
   def initialize(title)
     @title = title
-    # @backers = []
+    @backers = []
   end
 
   def add_backer(backer)
-    
+
 
   end
 
